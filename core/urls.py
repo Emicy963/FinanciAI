2f01fff062392b932e1ff37a23a92748510a138b
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-31 23:12:05 +0100

    Start the app accounts

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('credito.urls'))
+    path('', include('credito.urls')),
+    path('auth/', include('accounts.urls')),
 ]
 
 # Configuração para servir arquivos de mídia durante desenvolvimento
